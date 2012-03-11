628fab521d50c2f13f4f234c14cdf19ead3a7558
xissburg <xissburg@gmail.com>
2012-03-11 00:38:16 -0300

    It was jsut stupid to not use static table view cells -_________-

--- XBImageFilters/Classes/Sample/RootViewController.m
+++ XBImageFilters/Classes/Sample/RootViewController.m
@@ -8,18 +8,7 @@
 
 #import "RootViewController.h"
 
-#define kTitleKey @"title"
-#define kSegueKey @"segue"
-
-@interface RootViewController ()
-
-@property (copy, nonatomic) NSArray *sampleViewControllers;
-
-@end
-
 @implementation RootViewController
-
-@synthesize sampleViewControllers;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -32,9 +21,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.sampleViewControllers = [[NSArray alloc] initWithObjects:
-                                  [[NSDictionary alloc] initWithObjectsAndKeys:@"Image Filter", kTitleKey, @"ImageFilterSegue", kSegueKey, nil],
-                                  [[NSDictionary alloc] initWithObjectsAndKeys:@"Camera Filter", kTitleKey, @"CameraFilterSegue", kSegueKey, nil], nil];
 }
 
 - (void)viewDidUnload
@@ -45,29 +31,6 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-
-#pragma mark - UITableViewDataSource
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return self.sampleViewControllers.count;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
-    cell.textLabel.text = [[self.sampleViewControllers objectAtIndex:indexPath.row] objectForKey:kTitleKey];
-    return cell;
-}
-
-#pragma mark - UITableViewDelegate
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    
-    [self performSegueWithIdentifier:[[self.sampleViewControllers objectAtIndex:indexPath.row] objectForKey:kSegueKey] sender:nil];
 }
 
 @end

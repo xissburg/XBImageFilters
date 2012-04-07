8f98408481b7b6afe0270b2dbd19263bf3a98501
xissburg <xissburg@gmail.com>
2012-04-07 00:51:23 -0300

    Create BGRA bitmap context to support distinct pixel formats.

--- XBImageFilters/Classes/XBImageFilters/XBFilteredImageView.m
+++ XBImageFilters/Classes/XBImageFilters/XBFilteredImageView.m
@@ -28,20 +28,25 @@
         return;
     }
     
-    int width = CGImageGetWidth(image.CGImage);
-    int height = CGImageGetHeight(image.CGImage);
-    
-    CGSize imageSize = CGSizeMake(width/self.contentScaleFactor, height/self.contentScaleFactor);
-    UIGraphicsBeginImageContextWithOptions(imageSize, NO, self.contentScaleFactor);
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    CGContextScaleCTM(context, 1.f/self.contentScaleFactor, 1.f/self.contentScaleFactor);
-    CGContextDrawImage(context, CGRectMake(0, 0, width, height), image.CGImage);
+    // Create an RGBA bitmap context
+    CGImageRef CGImage = image.CGImage;
+    size_t width = CGImageGetWidth(CGImage);
+    size_t height = CGImageGetHeight(CGImage);
+    size_t bitsPerComponent = 8;
+    size_t bytesPerRow = width * 4;
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(NULL, width, height, bitsPerComponent, bytesPerRow, colorSpace, kCGImageAlphaPremultipliedFirst|kCGBitmapByteOrder32Little);
+    // Invert vertically for OpenGL
+    CGContextTranslateCTM(context, 0, height);
+    CGContextScaleCTM(context, 1, -1); 
+    CGContextDrawImage(context, CGRectMake(0, 0, width, height), CGImage);
     GLubyte *textureData = (GLubyte *)CGBitmapContextGetData(context);
     
     [self setContentSize:CGSizeMake(width, height)];
     [self _setTextureData:textureData width:width height:height];
     
-    UIGraphicsEndImageContext();
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
 }
 
 @end
